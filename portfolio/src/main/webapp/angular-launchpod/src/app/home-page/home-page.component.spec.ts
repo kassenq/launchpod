b1c31a168e44c40ecf373f707e5e31158ef27b54
Jag Jennings <jagjennings@google.com>
2020-06-22 12:08:52 -0700

    Replace homepage test.

--- portfolio/src/main/webapp/angular-launchpod/src/app/home-page/home-page.component.spec.ts
+++ portfolio/src/main/webapp/angular-launchpod/src/app/home-page/home-page.component.spec.ts
@@ -2,34 +2,24 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { HomePageComponent } from './home-page.component';
 
-// describe('HomePageComponent', () => {
-//   let component: HomePageComponent;
-//   let fixture: ComponentFixture<HomePageComponent>;
+describe('HomePageComponent', () => {
+  let component: HomePageComponent;
+  let fixture: ComponentFixture<HomePageComponent>;
 
-//   beforeEach(async(() => {
-//     TestBed.configureTestingModule({
-//       declarations: [ HomePageComponent ]
-//     })
-//     .compileComponents();
-//   }));
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ HomePageComponent ]
+    })
+    .compileComponents();
+  }));
 
-//   beforeEach(() => {
-//     fixture = TestBed.createComponent(HomePageComponent);
-//     component = fixture.componentInstance;
-//     fixture.detectChanges();
-//   });
+  beforeEach(() => {
+    fixture = TestBed.createComponent(HomePageComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
 
-//   it('should create', () => {
-//     expect(component).toBeTruthy();
-//   });
-// });
-
-import {Component} from '@angular/core';
-
-/** @title Simple form field */
-@Component({
-  selector: 'form-field-overview-example',
-  templateUrl: 'form-field-overview-example.html',
-  styleUrls: ['form-field-overview-example.css'],
-})
-export class FormFieldOverviewExample {}
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
